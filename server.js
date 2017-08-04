1ef5c480567ba4c7ede2f82e2be91a67c769e19d
dylan <gbujieshi@gmail.com>
2017-08-04 18:37:02 +0800

    post

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ var url= require("url");
 
 function start(route, handle){
 	function onRequest(request, response) {
-		
+		var postData = "";
 		var pathName=url.parse(request.url).pathname;
-		var content=route(handle, pathName, response);
-		// response.writeHead(200, {"Content-Type": "text/plain"});
-		// response.write(content);
-		// response.end();
+
+		console.log("Request for "+pathName+ " received.");
+
+		request.setEncoding("utf8");
+
+		request.addListener("data",function(postDataChunk){
+			postData+=postDataChunk;
+			console.log("Received POST data chunk '"+ postDataChunk+"'.")
+		})
+
+		request.addListener("end", function(){
+			route(handle, pathName, response, postData);
+		})
 	}
 
 	http.createServer(onRequest).listen(8888);
